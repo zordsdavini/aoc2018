18b0af5da37d0ed595d4f6d7f7f8eadd4ddd2a1a
Arnas Udovicius <arnas.u@estina.lt>
2018-12-12 10:16:58 +0200

    11 day working but not optimized

--- 11_2/run.py
+++ 11_2/run.py
@@ -18,41 +18,71 @@ class Grid:
                 if x not in self.grid:
                     self.grid[x] = {}
                 self.grid[x][y] = power
+                
+    def get_additional(self, x, y, size):
+        value = 0
+        for i in range(x, x + size):
+            value += self.grid[i][y + size - 1]
+        for i in range(y, y + size - 1):
+            value += self.grid[x + size - 1][i]
 
-    def get_power(self, x, y):
-        return self.grid[x][y]
+        return value
+
+
+def get_biggest_div(k):
+    for i in range(k - 1, 1, -1):
+        if k % i == 0:
+            return i
+    return 0
 
 
 print('Calculating power blocks...')
 grid = Grid(5177)
 
-pgrid = {}
-for size in range(1, 301):
-    for x in range(1, 301 - size):
-        for y in range(1, 301 - size):
-            power = 0
-            for i in range(size):
-                for j in range(size):
-                    power += grid.get_power(x + i, y + j)
-
-            if size not in pgrid:
-                pgrid[size] = {}
-                if x not in pgrid[size]:
-                    pgrid[size][x] = {}
-                pgrid[size][x][y] = power
-
 maximum = -20
 max_x = None
 max_y = None
-max_size = None
-for size in pgrid:
-    for x in pgrid[size]:
-        for y in pgrid[size][x]:
-            value = pgrid[size][x][y]
-            if value > maximum:
-                maximum = value
+max_size = 1
+for x in grid.grid:
+    for y in grid.grid[x]:
+        power = grid.grid[x][y]
+        if power > maximum:
+            maximum = power
+            max_x = x
+            max_y = y
+
+pgrid = {1: grid.grid}
+for size in range(2, 301):
+    # biggest_div = get_biggest_div(size)
+    # if biggest_div != 0:
+    #     dgrid = pgrid[biggest_div]
+    #     sdiv = size // biggest_div
+    # else:
+    #     dgrid = pgrid[size - 1]
+    #     sdiv = None
+
+    dgrid = pgrid[size - 1]
+    # print(size, biggest_div, len(grid.grid) + 1 - size)
+    print(size)
+    for x in range(1, len(grid.grid) + 1 - size):
+        for y in range(1, len(grid.grid) + 1 - size):
+            # if sdiv is not None:
+            #     power = 0
+            #     for i in range(sdiv):
+            #         for j in range(sdiv):
+            #             power += dgrid[x + i * sdiv][y + j * sdiv]
+            # else:
+            power = dgrid[x][y] + grid.get_additional(x, y, size)
+
+            if size not in pgrid:
+                pgrid[size] = {}
+            if x not in pgrid[size]:
+                pgrid[size][x] = {}
+            pgrid[size][x][y] = power
+            if power > maximum:
+                maximum = power
                 max_x = x
                 max_y = y
                 max_size = size
 
-print('Result:', max_x, max_y, 301 - max_size)
+print('Result:', max_x, max_y, max_size)
